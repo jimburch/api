9e0032d6ada9a8e9e87306bb39c001b3d14f5eca
Jim Burch <jrburch1@gmail.com>
2022-10-01 00:29:53 -0700

    setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
+import cors from "cors";
+
 import hoopsbotRouter from "./routes/hoopsbot.routes";
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+// Middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello World" });
 });
 
+// Routers
 app.use("/hoopsbot", hoopsbotRouter);
 
 app.listen(PORT, () => {
--- src/routes/hoopsbot.routes.ts
+++ src/routes/hoopsbot.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
+import { corsConfig } from "../utilities/cors";
 
 const hoopsbotRouter = express.Router();
 
-hoopsbotRouter.get("/", (req, res) => {
+hoopsbotRouter.get("/", cors(corsConfig), (req, res) => {
   res.send({ message: "NBA hot takes!" });
 });
 
