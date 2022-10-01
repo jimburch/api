6eb08d6e893b1552e32b3224e15090333d625b09
Jim Burch <jrburch1@gmail.com>
2022-09-30 18:22:07 -0700

    setup prettier and eslint, formatted repo

--- migrations/20220928060725_create_hoopsbot_takes_table.ts
+++ migrations/20220928060725_create_hoopsbot_takes_table.ts
@@ -1,16 +1,16 @@
-import { Knex } from 'knex';
+import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-	return await knex.schema.createTable('takes', t => {
-		t.increments('id');
-		t.string('take');
-		t.integer('hot').defaultTo(0);
-		t.integer('cold').defaultTo(0);
-		t.integer('shares').defaultTo(0);
-		t.timestamps(true, true);
-	});
+  return await knex.schema.createTable("takes", (t) => {
+    t.increments("id");
+    t.string("take");
+    t.integer("hot").defaultTo(0);
+    t.integer("cold").defaultTo(0);
+    t.integer("shares").defaultTo(0);
+    t.timestamps(true, true);
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-	return await knex.schema.dropTable('takes');
+  return await knex.schema.dropTable("takes");
 }
--- src/utilities/knex.ts
+++ src/utilities/knex.ts
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const NODE_ENV = process.env.NODE_ENV;
 
-export const knex = require('knex')({
-	client: 'pg',
-	connection: process.env.DB_URL,
-	debug: NODE_ENV === 'development',
-	ssl: NODE_ENV !== 'development' && {
-		sslmode: 'require',
-		rejectUnauthorized: false,
-	},
+export const knex = require("knex")({
+  client: "pg",
+  connection: process.env.DB_URL,
+  debug: NODE_ENV === "development",
+  ssl: NODE_ENV !== "development" && {
+    sslmode: "require",
+    rejectUnauthorized: false,
+  },
 });
