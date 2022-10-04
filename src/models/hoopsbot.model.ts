2d02f5db5b49e632ee23c5fcac7241c438b1321c
Jim Burch <jrburch1@gmail.com>
2022-10-04 12:34:57 -0700

    updated Take model and added text generation service function

--- src/models/hoopsbot.model.ts
+++ src/models/hoopsbot.model.ts
@@ -1,1 +1,49 @@
-// hoopsbot models
+export class Take {
+  take: string;
+  hot: number;
+  cold: number;
+  shares: number;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor(
+    take: string,
+    hot = 0,
+    cold = 0,
+    shares = 0,
+    created_at = new Date(),
+    updated_at = new Date()
+  ) {
+    this.take = take;
+    this.hot = hot;
+    this.cold = cold;
+    this.shares = shares;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
+
+  voteHot() {
+    this.hot++;
+    this.updated_at = new Date();
+  }
+
+  removeHot() {
+    this.hot--;
+    this.updated_at = new Date();
+  }
+
+  voteCold() {
+    this.cold++;
+    this.updated_at = new Date();
+  }
+
+  removeCold() {
+    this.cold--;
+    this.updated_at = new Date();
+  }
+
+  shareTake() {
+    this.shares++;
+    this.updated_at = new Date();
+  }
+}
--- src/services/hoopsbot.service.ts
+++ src/services/hoopsbot.service.ts
@@ -1,1 +1,42 @@
-// hoopsbot service functions
+import { Configuration, OpenAIApi } from "openai";
+import { knex } from "../utilities/knex";
+import { Take } from "../models/hoopsbot.model";
+
+const { OPENAI_AUTH, OPENAI_ORG } = process.env;
+
+const configuration = new Configuration({
+  organization: OPENAI_ORG,
+  apiKey: OPENAI_AUTH,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// "Write a controversial NBA hot take in the style of a tweet in 280 characters or less."
+export const generateTake = async (prompt: string): Promise<Take> => {
+  const completion: Take = await openai
+    .createCompletion({
+      model: "text-davinci-002",
+      prompt,
+      n: 1,
+      max_tokens: 500,
+      temperature: 0.99,
+      top_p: 1,
+      best_of: 1,
+    })
+    .then((response) => {
+      if (response.data.choices?.[0].text) {
+        return new Take(response.data.choices?.[0].text.replace(/\n/g, ""));
+      } else {
+        throw new Error("Completion choice text is undefined");
+      }
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+  if (!completion) throw new Error("Could not generate OpenAI text completion");
+  return completion;
+};
+
+// save new take to DB
+
+// update take in DB
