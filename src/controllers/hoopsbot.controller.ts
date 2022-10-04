0de077d9dbb8f088de61a0dc1283be562c3e9a6e
Jim Burch <jrburch1@gmail.com>
2022-10-04 13:11:34 -0700

    setup generate router and controller

--- src/controllers/hoopsbot.controller.ts
+++ src/controllers/hoopsbot.controller.ts
@@ -1,1 +1,18 @@
-// hoopsbot controllers
+import { Request, Response } from "express";
+import { Take } from "../models/hoopsbot.model";
+import { generateTake } from "../services/hoopsbot.service";
+
+export const generate = async (
+  req: Request,
+  res: Response
+): Promise<Response<Take>> => {
+  const prompt = req.query.prompt as string;
+  if (!prompt) return res.status(400).send("Missing text prompt");
+  try {
+    const take = await generateTake(prompt);
+    if (!take) return res.status(400).send("Could not generate take");
+    return res.status(200).send(take);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
--- src/routes/hoopsbot.routes.ts
+++ src/routes/hoopsbot.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { corsConfig } from "../utilities/cors";
 import passport from "passport";
+import { generate } from "../controllers/hoopsbot.controller";
 
 const hoopsbotRouter = express.Router();
 
@@ -12,6 +13,13 @@ hoopsbotRouter.get(
   async (req, res) => {
     res.send({ message: "NBA hot takes!" });
   }
+);
+
+hoopsbotRouter.get(
+  "/generate",
+  cors(corsConfig),
+  passport.authenticate("bearer", { session: false }),
+  async (req, res) => generate(req, res)
 );
 
 export default hoopsbotRouter;
