e217db3b294e6e8c3c99b41234b8952517599a3c
Jim Burch <jrburch1@gmail.com>
2022-10-10 15:52:30 -0700

    added deleted column and not null req to takes

--- migrations/20220928060725_create_hoopsbot_takes_table.ts
+++ migrations/20220928060725_create_hoopsbot_takes_table.ts
@@ -3,11 +3,12 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return await knex.schema.createTable("takes", (t) => {
     t.increments("id");
-    t.string("take");
+    t.string("take").notNullable();
     t.integer("hot").defaultTo(0);
     t.integer("cold").defaultTo(0);
     t.integer("shares").defaultTo(0);
     t.timestamps(true, true);
+    t.timestamp("deleted_at").defaultTo(null);
   });
 }
 
