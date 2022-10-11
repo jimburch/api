76e3be050a6f154ba4bef0dfad854065907edcbf
Jim Burch <49823725+JimBurch@users.noreply.github.com>
2022-10-10 21:07:49 -0700

    add cron job to save new taks (#10)

--- src/utilities/cron.ts
+++ src/utilities/cron.ts
@@ -1,8 +1,8 @@
-import { exit } from "process";
+import cron from "node-cron";
 import { saveNewTakeToDatabase } from "../services/hoopsbot.service";
 
-saveNewTakeToDatabase(
-  "Write a controversial NBA hot take in the style of a tweet in 280 characters or less."
-).then(() => {
-  exit();
+cron.schedule("0 12,16,20 * * *", () => {
+  saveNewTakeToDatabase(
+    "Write a controversial NBA hot take in the style of a tweet in 280 characters or less."
+  );
 });
