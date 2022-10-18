ca2e27fe06f67850b7f91c3e808452b0181cd24d
Jim Burch <jrburch1@gmail.com>
2022-10-17 17:25:14 -0700

    update cron to run each day at 1

--- src/utilities/cron.ts
+++ src/utilities/cron.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { tweetNewTake } from "../services/hoopsbot.service";
 
-cron.schedule("* */24 * * *", () => {
+cron.schedule("0 1 * * *", () => {
   tweetNewTake(
     "Write a controversial NBA hot take in the style of a tweet in 280 characters or less."
   );
