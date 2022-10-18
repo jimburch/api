1d7dbac2059f011cde9181606ac386c17deef37d
Jim Burch <jrburch1@gmail.com>
2022-10-18 09:03:43 -0700

    node env check for cron job

--- src/utilities/cron.ts
+++ src/utilities/cron.ts
@@ -3,10 +3,11 @@ import cron from "node-cron";
 import { tweetNewTake } from "../services/hoopsbot.service";
 
 const NODE_ENV = process.env.NODE_ENV;
-console.log(`NODE_ENV: ${NODE_ENV}`);
 
-cron.schedule("0 1 * * *", () => {
-  tweetNewTake(
-    "Write a controversial NBA hot take in the style of a tweet in 280 characters or less."
-  );
-});
+if (NODE_ENV === "production") {
+  cron.schedule("0 1 * * *", () => {
+    tweetNewTake(
+      "Write a controversial NBA hot take in the style of a tweet in 280 characters or less."
+    );
+  });
+}
